dad21d6e5ecb7e8c1c11f01b1770b2fd48525a4c
hasanmint73 <hasanmint73@gmail.com>
2022-05-24 10:17:40 +0600

    connected mongodb db connected

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
-
 
 //Middileware
 app.use(cors());
 app.use(express.json());
+
+//db connection
+
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cdeb8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('db connected');
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Test Manufacturer website Server !')
